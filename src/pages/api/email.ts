b004ea15c80ca6ae02c0428e88521281a5bc5236
marin <marinkitagawa.x86@gmail.com>
2023-06-14 02:35:08 -0400

    initial commit

---
+++ src/pages/api/email.ts
@@ -0,0 +1,66 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import axios from "axios";
+
+function getRequestParams(email: any) {
+  // get env variables
+  const API_KEY = "b80fbbcb679dcb911c1489eec4c82adb-us21";
+  const LIST_ID = "5cf5bf9c9f";
+  // mailchimp datacenter - mailchimp api keys always look like this:
+  // fe4f064432e4684878063s83121e4971-us6
+  // We need the us6 part
+  const DATACENTER = "us21";
+
+  const url = `https://${DATACENTER}.api.mailchimp.com/3.0/lists/${LIST_ID}/members`;
+
+  // Add aditional params here. See full list of available params:
+  // https://mailchimp.com/developer/reference/lists/list-members/
+  const data = {
+    email_address: email,
+    status: "subscribed",
+  };
+
+  // Api key needs to be encoded in base 64 format
+  const base64ApiKey = Buffer.from(`anystring:${API_KEY}`).toString("base64");
+  const headers = {
+    "Content-Type": "application/json",
+    Authorization: `Basic ${base64ApiKey}`,
+  };
+
+  return {
+    url,
+    data,
+    headers,
+  };
+}
+
+
+export default async(
+  req: NextApiRequest,
+  res: NextApiResponse
+) => {
+  
+
+  const { email } = req.body;
+
+  if (!email || !email.length) {
+    return res.status(400).json({
+      error: "Forgot to add your email?",
+    });
+  }
+
+  try {
+    const { url, data, headers } = getRequestParams(email);
+
+    const response = await axios.post(url, data, { headers });
+
+    // Success
+    return res.status(201).json({ error: null });
+  } catch (error) {
+    return res.status(400).json({
+      error: `not working`,
+    });
+
+    // Report error to Sentry or whatever
+}
+}

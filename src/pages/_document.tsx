bb766d9686709b19de31dece11a2b5129a6d1c85
marin <marinkitagawa.x86@gmail.com>
2023-06-14 15:31:33 -0400

    text color, icon fix

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head/>
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" href="/images/favicon.ico"/>
       <title>Site in progress!</title>
       <body>
         <Main />
